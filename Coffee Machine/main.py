265455e929d7812b9c18d3783495e352e703953f
benmahdjoubi <benmahdjoubi@outlook.com>
2020-07-04 16:46:07 +0100

    Update main.py

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -1,19 +1,40 @@
-# This is a program that simulates a coffee machine
+"""Coffee machine simulation
+To make coffee you need 200ml of water, 50ml of milk and 15g of beans""" 
 
+# Ask the user for how much water does he need
+print("Write how many ml of water the coffee machine has:")
+water = int(input("> "))
 
-# Ask the user for how many cups of coffee he needs
+# Ask the user for how much milk does he need 
+print("Write how many ml of milk the coffee machine has:")
+milk = int(input("> "))
+
+# Ask the user for how many grams of coffee beans he needs
+print("Write how many grams of coffee beans the coffee machine has:")
+beans = int(input("> "))
+
+# Ask the user of how many cups of coffee he needs
 print("Write how many cups of coffee you will need:")
-
-# Input from the command line
 cups = int(input("> "))
-print(f"For {cups} cups of coffee you will need:")
 
-# Calculate how much water, milk and coffee beans
-water = cups * 200
-milk = cups * 50
-coffee_beans = cups * 15
+# The values of water, milk and beans needed
+water_needed = 200
+milk_needed = 50
+beans_needed = 15
 
-# Print the total ammount of water, milk and coffee beans
-print(f"{water} ml of water")
-print(f"{milk} ml of milk")
-print(f"{coffee_beans} g of coffee beans")
+# The ratios of water, milk and beans
+water_ratio = water // water_needed
+milk_ratio = milk // milk_needed
+beans_ratio = beans // beans_needed
+
+# The min ratio
+min_ratio = min(water_ratio, milk_ratio, beans_ratio)
+
+if water_ratio > 0 and milk_ratio > 0 and beans_ratio > 0 and min_ratio < cups:
+  print(f"No, I can make only {min_ratio} cups of coffee")
+elif water_ratio > 0 and milk_ratio > 0 and beans_ratio > 0 and min_ratio > cups:
+  print(f"Yes, I can make that amount of coffee (and even {min_ratio-cups} more than that)")
+elif water_ratio == 0 and milk_ratio == 0 and beans_ratio == 0 and cups != 0:
+  print(f"No, I can make only {water_ratio} cups of coffee")
+else:
+  print("Yes, I can make that amount of coffee")
