3078cbc99169f68e81a9ee8bea3eb46b61663deb
benmahdjoubi <benmahdjoubi@outlook.com>
2020-07-11 10:48:29 +0100

    Adding new functionality like remaining and exit

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -1,142 +1,154 @@
 """Coffee machine simulation
-1- For one espresso, the coffee machine needs 250 ml of water and 16 g of coffee beans. 
+
+The user can buy, fill, take, see the remaining or exit from the coffee machine
+
+1- For one espresso, the coffee machine needs 250 ml of water and 16 g of coffee beans.
 It costs $4.
 2- For a latte, the coffee machine needs 350 ml of water, 75 ml of milk, and 20 g of coffee beans.
 It costs $7.
-3- And for a cappuccino, the coffee machine needs 200 ml of water, 100 ml of milk, and 12 g of coffee. 
+3- And for a cappuccino, the coffee machine needs 200 ml of water, 100 ml of milk, and 12 g of coffee.
 It costs $6.
 
-At the moment, the coffee machine has 
+At the moment, the coffee machine has
 $550, 400 ml of water, 540 ml of milk, 120 g of coffee beans, and 9 disposable cups
 """
 
-
-# Print the state of the coffee machine
-print("The coffee machine has:")
-print("400 of water")
-print("540 of milk")
-print("120 of coffee beans")
-print("9 of disposable cups")
-print("550 of money")
-print("")
-
-def buy(choice):
-  # Introduce the content of the coffee machine
-  water = 400  # in ml
-  milk = 540  # in ml
-  beans = 120  # in g
-  cups = 9
-  money = 550  # in $
-  
-  if choice == 1:
-    print(choice)
-    # After buying espresso
-    water -= 250
-    beans -= 16
-    milk = milk
-    money += 4
-    cups -= 1
-    
-  elif choice == 2:
-    # After buying latte
-    print(choice)
-    water -= 350
-    milk -= 75
-    beans -= 20
-    money += 7
-    cups -= 1
-  
-  elif choice == 3:
-    # After buying cappuccino
-    print(choice)
-    water -= 200
-    milk -= 100
-    beans -= 12
-    money += 6
-    cups -= 1
-  else:
-    pass
-
-  print("")
-  print("The coffee machine has:")
-  print(f"{water} of water")
-  print(f"{milk} of milk")
-  print(f"{beans} of coffee beans")
-  print(f"{cups} of disposable cups")
-  print(f"{money} of money")
-
-def fill():
-  # Introduce the content of the coffee machine
-  water = 400  # in ml
-  milk = 540  # in ml
-  beans = 120  # in g
-  cups = 9
-  money = 550  # in $
-
-  print("Write how many ml of water do you want to add:")
-  added_water = int(input("> "))
-  print("Write how many ml of milk do you want to add:")
-  added_milk = int(input("> "))
-  print("Write how many grams of coffee beans do you want to add:")
-  added_beans = int(input("> "))
-  print("Write how many disposable cups of coffee do you want to add:")
-  added_cups = int(input("> "))
-
-  # Update the values after the fill
-  water += added_water
-  milk += added_milk
-  beans += added_beans
-  cups += added_cups
-  money = money
-
-  print("")
-  print("The coffee machine has:")
-  print(f"{water} of water")
-  print(f"{milk} of milk")
-  print(f"{beans} of coffee beans")
-  print(f"{cups} of disposable cups")
-  print(f"{money} of money")
+# Import the sys library
+import sys
 
 
-def take():
-  # Introduce the content of the coffee machine
-  water = 400  # in ml
-  milk = 540  # in ml
-  beans = 120  # in g
-  cups = 9
-  money = 550  # in $
+def main():
+    # The state of the coffee machine
+    water = 400  # in ml
+    milk = 540  # in ml
+    beans = 120  # in g
+    cups = 9
+    money = 550  # in $
 
-  print(f"I gave you ${money}")
+    while True:
+        # Print the action to be done
+        print("Write action (buy, fill, take, remaining, exit):")
 
-  money -= money
+        # Input the function choice either buy,fill or take
+        choice1 = input("")
 
-  print("")
-  print("The coffee machine has:")
-  print(f"{water} of water")
-  print(f"{milk} of milk")
-  print(f"{beans} of coffee beans")
-  print(f"{cups} of disposable cups")
-  print(f"{money} of money")
+        # The choice is buy
+        if choice1 == "buy":
+            print("")
+            print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+            choice = input("")
+            # Go back to the buy menu
+            if choice == "back":
+                main()
 
-# Print the action to be done
-print("Write action (buy, fill, take):")
+            elif int(choice) == 1 and water >= 250 and beans >= 16:
+                # After buying espresso
+                water -= 250
+                beans -= 16
+                money += 4
+                cups -= 1
 
-# Input the function choice either buy,fill or take
-choice1 = input("> ")
+                print("")
+                print("I have enough resources, making you a coffee!")
 
-# The choice is buy
-if choice1 == "buy":
-  print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-  choice2 = int(input("> "))
-  buy(choice2)
+            elif int(choice) == 2 and water >= 350 and milk >= 75 and beans >= 20:
+                # After buying latte
+                water -= 350
+                milk -= 75
+                beans -= 20
+                money += 7
+                cups -= 1
+
+                print("")
+                print("I have enough resources, making you a coffee!")
+
+            elif int(choice) == 3 and water >= 200 and milk >= 100 and beans >= 12:
+                # After buying cappuccino
+                water -= 200
+                milk -= 100
+                beans -= 12
+                money += 6
+                cups -= 1
+
+                print("I have enough resources, making you a coffee!")
+                print("")
+
+            elif int(choice) == 1 and water < 250:
+                print("Sorry, not enough water!")
+                print("")
+
+            elif int(choice) == 1 and beans < 16:
+                print("Sorry, not enough coffee beans!")
+                print("")
+
+            elif int(choice) == 2 and water < 350:
+                print("Sorry, not enough water!")
+                print("")
+
+            elif int(choice) == 2 and milk < 75:
+                print("Sorry, not enough milk!")
+                print("")
+
+            elif int(choice) == 2 and milk < 20:
+                print("Sorry, not enough coffee beans!")
+                print("")
+
+            elif int(choice) == 3 and water < 200:
+                print("Sorry, not enough water!")
+                print("")
+
+            elif int(choice) == 3 and milk < 100:
+                print("Sorry, not enough milk!")
+                print("")
+
+            elif int(choice) == 3 and beans < 12:
+                print("Sorry, not enough coffee beans!")
+                print("")
+
+            else:
+                pass
+
+        # The choice is fill
+        elif choice1 == "fill":
+            # Introduce the content of the coffee machine
+            print("Write how many ml of water do you want to add:")
+            added_water = int(input(""))
+            print("Write how many ml of milk do you want to add:")
+            added_milk = int(input(""))
+            print("Write how many grams of coffee beans do you want to add:")
+            added_beans = int(input(""))
+            print("Write how many disposable cups of coffee do you want to add:")
+            added_cups = int(input(""))
+
+            # Update the values after the fill
+            water += added_water
+            milk += added_milk
+            beans += added_beans
+            cups += added_cups
+
+        # The choice is take
+        elif choice1 == "take":
+
+            print("")
+            print(f"I gave you ${money}")
+            money -= money
+
+        elif choice1 == "remaining":
+            print("")
+            print("The coffee machine has:")
+            print(f"{water} of water")
+            print(f"{milk} of milk")
+            print(f"{beans} of coffee beans")
+            print(f"{cups} of disposable cups")
+            print(f"{money} of money")
+            print("")
+
+        elif choice1 == "exit":
+            sys.exit()
+
+        else:
+            pass
 
 
-# The choice is fill
-elif choice1 == "fill":
-  # Introduce the content of the coffee machine
-  fill()
-
-# The choice is take
-else:
-  take()
-
+if __name__ == "__main__":
+    main()
